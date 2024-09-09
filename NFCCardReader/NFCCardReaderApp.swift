4f4cb63699affb0e20cb67d79eecfa8479bf3554
Kobiraj <mmoon@laurentian.ca>
2024-09-08 22:41:06 -0400

    Initial Commit

---
+++ NFCCardReader/NFCCardReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCCardReaderApp.swift
+//  NFCCardReader
+//
+//  Created by Mahedee Zaman on 08/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct NFCCardReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
